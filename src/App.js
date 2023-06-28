cbb15b2f404aebcd39d6b6eebde35df6c3877af3
AlisaCosmos <57442445+AlisaCosmos@users.noreply.github.com>
2023-06-28 13:51:04 +0300

    Проверка

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export const SearchContext = createContext();
 
 function App() {
   const [searchValue, setSearchValue] = useState([]);
-
+ const a =
   return (
     <div className="App">
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
